86bb8750077662df25ccf02befe93021fb55f412
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-05-19 15:04:10 -0600

    Rails Create functionality finished.

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,4 +3,15 @@ class IdeasController < ApplicationController
     @ideas = Idea.all
     @idea = Idea.new
   end
+
+  def create
+    Idea.create(idea_params)
+    redirect_to ideas_path
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:title, :body)
+  end
 end
--- test/features/guest_creating_idea_test.rb
+++ test/features/guest_creating_idea_test.rb
@@ -11,4 +11,16 @@ class GuestCreatingIdeaTest < ActionController::TestCase
       assert page.has_button?("Save New Idea", disabled: false)
     end
   end
+
+  test "A guest can add an idea and it is added to the database" do
+    visit '/ideas'
+
+    within(".new_idea") do
+     fill_in "New Title", with: "Test title"
+     fill_in "New Body", with: "Body test"
+      assert_difference('Idea.count', 1) do
+        click_link_or_button("Save New Idea")
+      end
+    end
+  end
 end
