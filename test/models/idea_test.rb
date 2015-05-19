86066f14b3fefb521d15ac4f071b6511fa45bc5e
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-05-18 19:18:33 -0600

    Tests quality method on idea.
    The method is simple enough, but being an else if statement it violates one of Metz's 4 rules. A sleek refactoring isn't immediately coming to mind, so I'll worry about it later. Validation testing next.

--- test/models/idea_test.rb
+++ test/models/idea_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class IdeaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "Idea's status is made into a funny quality" do
+     swill = Idea.find_by(status: 0)
+     hooch = Idea.find_by(status: 1)
+     cordial = Idea.find_by(status: 2)
+
+     assert_equal "Scallywag's Swill!", swill.quality
+     assert_equal "Powder Monkey's Hooch", hooch.quality
+     assert_equal "Old Salt's Cordial!", cordial.quality
+     refute_equal "Powder Monkey's Hooch", swill.quality
+   end
 end
